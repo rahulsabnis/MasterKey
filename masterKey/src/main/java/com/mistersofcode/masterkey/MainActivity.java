aef4ea7eac1498284e6dc9b728ac3a40214cda66
Rahul Sabnis <rahulsabnis19@gmail.com>
2015-08-22 13:54:18 -0700

    Push Generated IDE Files

--- masterKey/src/main/java/com/mistersofcode/masterkey/MainActivity.java
+++ masterKey/src/main/java/com/mistersofcode/masterkey/MainActivity.java
@@ -2,38 +2,16 @@ package com.mistersofcode.masterkey;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import com.simplify.android.sdk.Simplify;
 
 
 public class MainActivity extends Activity {
+
+    private Simplify mSimplify = new Simplify("sbpb_ZTQ4ZjEyZDItOTM5Zi00NDcxLWIyMmQtOTc4YWExYTRkZWQy");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 }
