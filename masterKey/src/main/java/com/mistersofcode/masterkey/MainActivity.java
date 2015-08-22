c20e63ebcda115e7071437d4079b6c5da84cc3bb
Rahul Sabnis <rahulsabnis19@gmail.com>
2015-08-22 14:57:41 -0700

    Add Simplify Commerce Payment Layout

--- masterKey/src/main/java/com/mistersofcode/masterkey/MainActivity.java
+++ masterKey/src/main/java/com/mistersofcode/masterkey/MainActivity.java
@@ -2,8 +2,12 @@ package com.mistersofcode.masterkey;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import com.parse.Parse;
 import com.simplify.android.sdk.Simplify;
+import com.simplify.android.sdk.model.SimplifyError;
+import com.simplify.android.sdk.model.Token;
+import com.simplify.android.sdk.view.CardEditor;
 
 
 public class MainActivity extends Activity {
@@ -16,5 +20,17 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "oeMDj84i1tC5FaWLTf3X0InyDn3ahWIK7zM6xfVj", "8kXcxnLbLBndSj2oq8Y0TUiKFXT6buQDecyMp4L4");
+        Simplify.CreateTokenListener listener = new Simplify.CreateTokenListener() {
+            @Override
+            public void onSuccess(Token token) {
+                Log.i("Simplify", "Created Token: " + token.getId());
+            }
+
+            @Override
+            public void onError(SimplifyError simplifyError) {
+                Log.e("Simplify", "Error Creating Token: " + simplifyError.getMessage());
+            }
+        };
+        CardEditor editor = (CardEditor) findViewById(R.id.card_editor);
     }
 }
