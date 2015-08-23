3f6fc73a51bfd33d76466d32d31c6604d7dfd995
Rahul Sabnis <rahulsabnis19@gmail.com>
2015-08-22 23:29:59 -0700

    Add PreferencesFragment Source File

---
+++ masterKey/src/main/java/com/mistersofcode/masterkey/PreferencesFragment.java
@@ -0,0 +1,26 @@
+package com.mistersofcode.masterkey;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by rahulsabnis on 8/22/15.
+ */
+public class PreferencesFragment extends Fragment {
+
+    public PreferencesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_preferences, container, false);
+    }
+
+
+}
