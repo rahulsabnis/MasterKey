3fc088e979de6789eb15d6a47e7fbd3c32904eca
Rahul Sabnis <rahulsabnis19@gmail.com>
2015-08-22 17:15:39 -0700

    Add Reset Button and Roll Back Build Tools Version from 23 to 22.0.1

--- masterKey/src/main/java/com/mistersofcode/masterkey/MainActivity.java
+++ masterKey/src/main/java/com/mistersofcode/masterkey/MainActivity.java
@@ -1,10 +1,12 @@
 package com.mistersofcode.masterkey;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import com.parse.Parse;
 import com.parse.ParseObject;
 import com.simplify.android.sdk.Simplify;
@@ -16,40 +18,67 @@ import com.simplify.android.sdk.view.CardEditor;
 
 public class MainActivity extends Activity {
 
-    private Simplify mSimplify = new Simplify("sbpb_ZTQ4ZjEyZDItOTM5Zi00NDcxLWIyMmQtOTc4YWExYTRkZWQy");
+    private String SIMPLIFY_PUBLIC_API_KEY = "sbpb_ZTQ4ZjEyZDItOTM5Zi00NDcxLWIyMmQtOTc4YWExYTRkZWQy";
+
+    private String SIMPLIFY_PRIVATE_API_KEY = "xKixB/deX5d7CdKAj7uWHdNAH2KtglPu76Siwzl25ax5YFFQL0ODSXAOkNtXTToq";
+
+    private Simplify mSimplify;
+
+    private CardEditor mCardEditor;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSimplify = new Simplify(SIMPLIFY_PUBLIC_API_KEY);
         Parse.enableLocalDatastore(this);
         final CardEditor editor = (CardEditor) findViewById(R.id.card_editor);
         Parse.initialize(this, "oeMDj84i1tC5FaWLTf3X0InyDn3ahWIK7zM6xfVj", "8kXcxnLbLBndSj2oq8Y0TUiKFXT6buQDecyMp4L4");
-        final Simplify.CreateTokenListener listener = new Simplify.CreateTokenListener() {
-            @Override
-            public void onSuccess(Token token) {
-                Log.i("Simplify", "Created Token: " + token.getId());
-                editor.showSuccessOverlay("Payment Successful");
-            }
+        initUI();
+    }
 
+    private void initUI()
+    {
+        mCardEditor = (CardEditor) findViewById(R.id.card_editor);
+
+        mCardEditor.setOnChargeClickListener(new View.OnClickListener() {
             @Override
-            public void onError(SimplifyError simplifyError) {
-                Log.e("Simplify", "Error Creating Token: " + simplifyError.getMessage());
-                editor.showErrorOverlay("Payment Failed");
-            }
-        };
-        editor.setOnChargeClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Card card = editor.getCard();
-                AsyncTask<?, ?, ?> createTokenTask = mSimplify.createCardToken(card, listener);
-                ParseObject transaction = new ParseObject("Transaction");
-                transaction.put("Card Number", editor.getCard().getNumber());
-                transaction.put("Charity Phrase", "Unknown");
-                // TODO More Fields
-                transaction.saveInBackground();
+            public void onClick(View view) {
+                // hide keyboard
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
+                Card card = mCardEditor.getCard();
+                AsyncTask<?, ?, ?> task = mSimplify.createCardToken(card, new Simplify.CreateTokenListener() {
+                    @Override
+                    public void onSuccess(Token token) {
+                        Log.i(TAG, "Created Token: " + token.getId());
+                        mCardEditor.showSuccessOverlay("");
+//                        Log.d("", token.getId());
+                        ParseObject transaction = new ParseObject("Transaction");
+                        transaction.put("Card Number", token.getCard().getNumber());
+                        transaction.put("Charity Phrase", "Unknown");
+                        // TODO More Fields
+                        transaction.saveInBackground();
+                    }
+
+                    @Override
+                    public void onError(SimplifyError error) {
+                        Log.e(TAG, "Error Creating Token: " + error.getMessage());
+                        mCardEditor.showErrorOverlay("Unable to retrieve card token. " + error.getMessage());
+                    }
+                });
             }
         });
 
+        // init reset button
+        findViewById(R.id.btnReset).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCardEditor.reset();
+            }
+        });
     }
 }
